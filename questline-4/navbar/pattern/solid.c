be5bbcb58e7dbf5dcd6cdd11e6b3bed9e9da4340
mival236 <mivalmaria2006@gmail.com>
2025-09-14 00:18:11 +0530

    completed

---
+++ questline-4/navbar/pattern/solid.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+intmain(){
+    int i,j,n;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    for(i=1; i<=n;i++)
+    {
+        for(j=1;j<=5,j++)
+        printf("*\n")
+    }  
+    return 0;   
+}
